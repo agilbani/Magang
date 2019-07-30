ea66a78c62625988817f9edf85609274c6fa986d
Realfianda <restyalfiandaa@gmail.com>
2019-07-30 10:17:49 +0700

    menambahkan scroll view pada layout register

--- app/src/main/java/com/example/marlin/magang/Register.java
+++ app/src/main/java/com/example/marlin/magang/Register.java
@@ -35,7 +35,7 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.register);
 
         nama = (EditText) findViewById(R.id.etName);
         email = (EditText) findViewById(R.id.etEmail);
