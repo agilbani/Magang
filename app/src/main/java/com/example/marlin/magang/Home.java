b0ff682f4951745f83b05e86f9f0aefc6cb6dfb9
ratihtriutami <ratihtetehlova@gmail.com>
2019-08-08 17:50:11 +0700

    application almost done!

--- app/src/main/java/com/example/marlin/magang/Home.java
+++ app/src/main/java/com/example/marlin/magang/Home.java
@@ -183,6 +183,7 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
+                startActivity(new Intent(Home.this, Home.class));
             }
         });
 
